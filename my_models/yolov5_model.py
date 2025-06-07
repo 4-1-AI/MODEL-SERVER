6a2dc5f44a85d7ce6d834f6445bb12539d29552e
minju <minju9711@naver.com>
2025-06-08 06:52:55 +0900

    feat: yolov5 레포 기반 코드 수정정

--- my_models/yolov5_model.py
+++ my_models/yolov5_model.py
@@ -16,7 +16,7 @@ from torchvision import transforms
 from utils.general import non_max_suppression
 
 model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "fire_smoke.pt"))
-model = YOLO(model_path, device="cpu")
+model = YOLO(model_path)
 
 transform = transforms.Compose([
     transforms.Resize((640, 640)),
